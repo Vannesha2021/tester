ee2f9f17a4590ceb4a1673b174477cbcc5714120
Vannesha <vannesha.dev@gmail.com>
2022-01-15 22:57:04 +0000

    tried but didt work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,12 @@
+document.getElementById("question").innerHTML = "What is the deepest ocean?"
 
 
-function QuestionAnswer1 (question, answer) {
-    this.question = question;
-    this.answer = answer;
-    this.getQuestion = function () {
-        return this.getQuestion();
-    }
-}
 
-const QuestionAnswer1a = new question ('What is the deepest ocean?')
 
-console.log(QuestionAnswer1a.getQuestion());
+
+
+
+
 
 
 
