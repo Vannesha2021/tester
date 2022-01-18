64964492a7ecaf42e8159387ce4b5a794b67f4b0
Vannesha <vannesha.dev@gmail.com>
2022-01-18 18:13:37 +0000

    Js works and questions has been added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,65 @@
-quiz : [
+let quiz = {
+  quiz : [ 
     {
-      question : "What is the standard distance between the target and archer in Olympics?",
+      question : "Which canal separates the African continent from Asia?",
       answer : [
-        "50 meters",
-        "70 meters",
-        "100 meters",
-        "120 meters"
-      ],
-      correct : 1 // arrays start with 0, so answer is 70 meters
-    },
-    {
-        question : "Which is the highest number on a standard roulette wheel?",
-        answer : [
-        "22",
-        "24",
-        "32",
-        "36"
+        "Rhine-Main-Danube Canal",
+        "Volga-Don Canal",
+        "Danube-Black Sea Canal",
+        "Suez Canal"
       ],
       correct : 3
     },
     {
-        question : "How much wood could a woodchuck chuck if a woodchuck would chuck wood?",
+        question : "Which continent is Kazakhstan located at?",
         answer : [
-        "400 pounds",
-        "550 pounds",
-        "700 pounds",
-        "750 pounds"
+        "Europe",
+        "Africa",       
+        "Asia",
+        "South America"
       ],
       correct : 2
     },
     {
-        question : "Which is the seventh planet from the sun?",
+        question : "Which of these famous rivers do not have bridges?",
         answer : [
-        "Uranus",
-        "Earth",
-        "Pluto",
-        "Mars"
+        "Ganges River",
+        "Amazon River",
+        "Nile River",
+        "Ohio River"
       ],
-      correct : 0
+      correct : 1
     },
     {
-        question : "Which is the largest ocean on Earth?",
-        answer : [
-        "Atlantic Ocean",
-        "Indian Ocean",
-        "Arctic Ocean",
-        "Pacific Ocean"
+      question : "Which of the following countries is the production and sale of tobacco/cigarettes banned?",
+      answer : [
+      "Ireland",
+      "Malaysia",
+      "Bhutan",
+      "Japan"
       ],
-      correct : 3
+      correct : 2
+    },
+    {
+        question : "What is the percentage of population for Europe?",
+        answer : [
+        "17.20%",
+        "9.59%",
+        "7.60%",
+        "20.6%"
+      ],
+      correct : 1
     }
-    ]
+    ],
 
-quizHeart = null,
-question = null, // HTML question wrapper
-answer = null, // HTML answers wrapper
+quizHeart : null,
+question : null, // HTML question wrapper
+answer : null, // HTML answers wrapper
+nextButton : null,
+now : 0, // current question
+score : 0, // current score
 
-//score
-now = 0, // current question
-score = 0 // current score
-
-quizContainer : () => {
+init: () => {
     quiz.quizHeart = document.getElementById("questionContainer")
 
 quiz.question = document.createElement("div");
@@ -72,58 +72,62 @@ quiz.quizHeart.appendChild(quiz.answer);
 
 // start quiz
 quiz.draw();
+},
 
-window.addEventListener("load", quiz.quizContainer);
-}
-
-createQuiz : () => {
-    quiz.question.innerHTML = quiz.quiz[quiz.now].q;
+draw: () => {
+    quiz.question.innerHTML = quiz.quiz[quiz.now].question;
 
     quiz.answer.innerHTML = "";
-    for (let i in quiz.quiz[quiz.now].o) {
+    for (let i in quiz.quiz[quiz.now].answer) {
         let radio = document.createElement("input");
         radio.type = "radio";
         radio.name = "quiz";
-        radio.id = "quizo" + i;
+        radio.id = "quizanswer" + i;
         quiz.answer.appendChild(radio);
         let label = document.createElement("label");
-        label.innerHTML = quiz.quiz[quiz.now].o[i];
-        label.setAttribute("for", "quizo" + i);
+        label.innerHTML = quiz.quiz[quiz.now].answer[i];
+        label.setAttribute("for", "quizanswer" + i);
         label.dataset.idx = i;
         label.addEventListener("click", () => { quiz.select(label); });
-        quiz.answers.appendChild(label);
-    }
-}
+        quiz.answer.appendChild(label);
 
-// (D) OPTION SELECTED
+    }
+},
 
 select: (answer) => {
-    let all = quiz.answer.getElementsByTagName("label");
-    for (let label of all) {
+    let questions = quiz.answer.getElementsByTagName("label");
+    for (let label of questions) {
         label.removeEventListener("click", quiz.select);
       }
-      let correct = option.dataset.idx == quiz.quiz[quiz.now].a;
+      let correct = answer.dataset.idx == quiz.quiz[quiz.now].correct;
       if (correct) {
         quiz.score++;
-        option.classList.add("correct");
+        answer.classList.add("correct");
+        alert("That's correct!");
     } else {
-        option.classList.add("wrong");
+        answer.classList.add("wrong");
+        alert("Oh NO!");
       }
       quiz.now++;
       setTimeout(() => {
-      if (quiz.now < quiz.data.length) { quiz.draw(); }
+      if (quiz.now < quiz.quiz.length) { quiz.draw(); }
       else {
-        quiz.question.innerHTML = `You have answered ${quiz.score} of ${quiz.data.length} correctly.`;
+        quiz.question.innerHTML = `You have answered ${quiz.score} of ${quiz.quiz.length} correctly.`;
         quiz.answer.innerHTML = "";
       }
     }, 1000);
-}
+},
 
 reset : () => {
   quiz.now = 0;
   quiz.score = 0;
   quiz.draw();
 }
+};
+
+window.addEventListener("load", quiz.init);
+
+
 
 
 
